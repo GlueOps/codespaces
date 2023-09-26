27c98c6cd989dda8ef0630786ff3af3912247045
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-09-26 05:25:36 +0000

    chore: better names, cleanup

--- .devcontainer/tools/add-tools-to-shells.sh
+++ .devcontainer/tools/add-tools-to-shells.sh
@@ -3,9 +3,9 @@
 tools_file="/etc/tools.sh"
 
 add_tools_to_shell() {
-    local file="$1"
-    if [ -f "$file" ]; then
-        echo "source $tools_file" >> "$file"
+    local rc_file="$1"
+    if [ -f "$rc_file" ]; then
+        echo "source $tools_file" >> "$rc_file"
     fi
 }
 
@@ -14,7 +14,3 @@ add_tools_to_shell "$HOME/.bashrc"
 
 # zsh
 add_tools_to_shell "$HOME/.zshrc"
-
-
-# Add more shells as needed
-# add_tools_to_shells "shell_cmd" "shell_config_file" "shell_global_config_file"
