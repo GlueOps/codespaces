890a51acfef2474fdd346bfd94cd236d933eb8dc
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-10-07 16:52:38 +0000

    fix: avoid other file

--- .devcontainer/tools/add-tools-to-shells.sh
+++ .devcontainer/tools/add-tools-to-shells.sh
@@ -2,12 +2,12 @@
 
 add_tools_to_shells() {
     if [ -f "/home/vscode/.bashrc" ]; then
-        find /tmp/tools/ -type f ! -name "add-tools-to-shells.sh" \
+        find /tmp/tools/ -type f ! -name "add-tools-to-shells.sh" ! -name "helm-repositories.yaml" \
             -exec sh -c 'echo >> /home/vscode/.bashrc; cat "{}" >> /home/vscode/.bashrc' \;
     fi
 
     if [ -d "/home/vscode/.oh-my-zsh/custom" ]; then
-        find . -type f ! -name "add-tools-to-shells.sh" -name "*.sh" \
+        find . -type f ! -name "add-tools-to-shells.sh" ! -name "helm-repositories.yaml" -name "*.sh" \
             -exec sh -c 'file="{}"; cp "$file" "/home/vscode/.oh-my-zsh/custom/$(basename ${file%.sh}.zsh)"' \;
     fi
 
