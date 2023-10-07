7033a1b2232fc7a3663538b86cc1fce63e89e3af
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-10-07 14:55:46 +0000

    feat: accommodate bash and zsh

---
+++ .devcontainer/tools/add-tools-to-shells.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+add_tools_to_shells() {
+    if [ -f "/home/vscode/.bashrc" ]; then
+        find /tmp/tools/ -type f ! -name "add-tools-to-shells.sh" \
+            -exec sh -c 'echo >> /home/vscode/.bashrc; cat "{}" >> /home/vscode/.bashrc' \;
+    fi
+
+    if [ -d "/home/vscode/.oh-my-zsh/custom" ]; then
+        find . -type f ! -name "add-tools-to-shells.sh" -name "*.sh" \
+            -exec sh -c 'file="{}"; cp "$file" "/home/vscode/.oh-my-zsh/custom/$(basename ${file%.sh}.zsh)"' \;
+    fi
+
+}
+
+add_tools_to_shells
---
+++ .devcontainer/tools/glueops-fetch-repos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+glueops-fetch-repos() {
+    #https://stackoverflow.com/a/68770988/4620962
+    
+    gh repo list $(git remote get-url origin | cut -d/ -f4) --no-archived --limit 1000 | while read -r repo _; do
+      gh repo clone "$repo" "$repo" -- --depth=1 --recurse-submodules || {
+        git -C $repo pull
+      } &
+    done
+}
