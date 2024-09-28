ec112b6b874bf95144983395fcceed825f2f40ce
Venkat <venkata@venkatamutyala.com>
2024-09-28 01:48:04 +0000

    feat: adding aws

--- os-setup.sh
+++ os-setup.sh
@@ -2,10 +2,10 @@
 set -e -o pipefail
 
 echo "waiting for cloud-init to finish..."
-cloud-init status --wait
+sudo cloud-init status --wait
 
 echo "installing packages..."
-apt-get update
+sudo apt-get update
 echo "installing tailscale"
 curl -fsSL https://tailscale.com/install.sh | sh
 curl -sL setup.glueops.dev | sh
@@ -13,5 +13,9 @@ curl -sL setup.glueops.dev | sh
 # My setup...
 
 echo "cleaning up"
-cloud-init clean --machine-id --seed --logs
-rm -rvf /var/lib/cloud/instances /etc/machine-id /var/lib/dbus/machine-id /var/log/cloud-init*
+sudo cloud-init clean --machine-id --seed --logs
+sudo rm -rvf /var/lib/cloud/instances /etc/machine-id /var/lib/dbus/machine-id /var/log/cloud-init*
+# AWS seems to have issues if the /etc/machine-id file is removed
+sudo touch /etc/machine-id
+sudo rm /root/.ssh/authorized_keys | true
+sudo rm /home/admin/.ssh/authorized_keys | true
