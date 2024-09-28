4d385dd44b6f0f6138a0f7c21a3a00cda6550268
Venkat <venkata@venkatamutyala.com>
2024-09-27 19:19:24 -0700

    fix: rm of authorized keys

--- os-setup.sh
+++ os-setup.sh
@@ -17,5 +17,5 @@ sudo cloud-init clean --machine-id --seed --logs
 sudo rm -rvf /var/lib/cloud/instances /etc/machine-id /var/lib/dbus/machine-id /var/log/cloud-init*
 # AWS seems to have issues if the /etc/machine-id file is removed
 sudo touch /etc/machine-id
-sudo rm /root/.ssh/authorized_keys | true
-sudo rm /home/admin/.ssh/authorized_keys | true
+sudo rm /root/.ssh/authorized_keys || true
+sudo rm /home/admin/.ssh/authorized_keys || true
