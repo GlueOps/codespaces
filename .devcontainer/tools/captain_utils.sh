d101beef75cbd738e7ec82f9b3bc54fc80b51d0f
Bazmahou <hamzabouissi@sousse.r-iset.tn>
2025-06-16 19:29:36 +0000

    fix: change chart.yaml and grouped comp

--- .devcontainer/tools/captain_utils.sh
+++ .devcontainer/tools/captain_utils.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+environment=production
 BUCKET_NAME="helm-diff"
 CAPTAIN_CLUSTER_NAME=$(basename $(pwd))
 
@@ -8,15 +8,14 @@ helm repo update
 upload_diff() {
     gum log --structured --level info "Uploading helm-diff output to ..."
 }
-environment=production
 
 # Function to handle version selection and helm upgrade
 handle_helm_upgrades() {
     local component=$1
     # Handle exit option
     if [ "$environment" = "production" ]; then
-        local argocd_version=($(cat VERSIONS/charts.yaml | yq '.versions.argocd_helm_chart_version'))
-        local platform_version_string=($(cat VERSIONS/charts.yaml | yq '.versions.glueops_platform_helm_chart_version'))
+        local argocd_version=($(cat VERSIONS/glueops.yaml | yq '.versions.argocd_helm_chart_version'))
+        local platform_version_string=($(cat VERSIONS/glueops.yaml | yq '.versions.glueops_platform_helm_chart_version'))
     else
         local argocd_version=($(helm search repo  argo/argo-cd --versions -o json | jq -r "limit(30; .[]).version" | paste -sd' ' -)) 
         local platform_version_string=$(gh release list --repo GlueOps/platform-helm-chart-platform --limit 10 --json tagName --jq '.[].tagName' | paste -sd' ' -)
@@ -72,13 +71,24 @@ handle_helm_upgrades() {
             local argocd_crd_versions=($(helm search repo argo/argo-cd --versions -o json | jq --arg chart_helm_version "$version" -r '.[] | select(.version == $chart_helm_version).app_version' | sed 's/^v//'))
             chosen_crd_version=$(gum choose "${argocd_crd_versions[@]}" "Back")
             pre_commands="kubectl apply -k \"https://github.com/argoproj/argo-cd/manifests/crds?ref=v$chosen_crd_version\" && helm repo update"
+            # Check if user wants to go back
+            if [ "$chosen_crd_version" = "Back" ]; then
+                return
+            fi
         fi
-
-        # Check if user wants to go back
-        if [ "$chosen_crd_version" = "Back" ]; then
+        
+        set -x
+        eval "$helm_diff_cmd | gum pager" # Execute the main helm diff command
+        gum style --bold --foreground 212 "✅ Diff complete."
+        set +x
+        
+        if ! gum confirm "Apply upgrade"; then
             return
         fi
-
+        
+        # Running helm diff command
+        gum style --bold "The following commands will be executed:"
+        
         # New: Execute pre_commands if defined
         if [ -n "$pre_commands" ] && [ -n "$chosen_crd_version" ]; then
             gum style --bold "Executing pre-commands for $component:"
@@ -92,16 +102,6 @@ handle_helm_upgrades() {
             set +x
             gum style --bold --foreground 212 "✅ Pre-commands complete."
         fi
-        set -x
-       
-        eval "$helm_diff_cmd | gum pager" # Execute the main helm diff command
-        gum style --bold --foreground 212 "✅ Diff complete."
-
-        if ! gum confirm "Apply upgrade"; then
-            return
-        fi
-        # Running helm diff command
-        gum style --bold "The following commands will be executed:"
         set -x
         helm upgrade --install "$component" "$chart_name" --version "$version" -f "$target_file" -f "$overrides_file" -n "$namespace" --create-namespace --skip-crds
         set +x
@@ -123,6 +123,30 @@ handle_kubernetes_version() {
     "${command_args[@]}"
 }
 
+
+handle_aws_options(){
+    local aws_component=$(gum choose "eks-addons" "upgrade-eks-nodepools" "upgrade-kubernetes" "Exit")
+    # Handle exit option
+    if [ "$aws_component" = "Exit" ]; then
+        echo "Goodbye!"
+        exit 0
+    fi
+
+    if [ "$aws_component" = "eks-addons" ]; then
+        handle_terraform_addons
+    fi
+
+    if [ "$aws_component" = "upgrade-eks-nodepools" ]; then
+        handle_terraform_nodepools
+    fi
+    
+    if [ "$aws_component" = "upgrade-kubernetes" ]; then
+        handle_kubernetes_version
+    fi
+
+   
+}
+
 # Main menu loop
 while true; do
     # Show main menu
@@ -134,7 +158,7 @@ while true; do
         exit 0
     fi
     
-    component=$(gum choose "argocd" "glueops-platform" "eks-addons" "upgrade-eks-nodepools" "upgrade-kubernetes" "Exit")
+    component=$(gum choose "argocd" "glueops-platform" "aws" "Exit")
     
     # Handle exit option
     if [ "$component" = "Exit" ]; then
@@ -142,16 +166,10 @@ while true; do
         exit 0
     fi
 
-    if [ "$component" = "eks-addons" ]; then
-        handle_terraform_addons
-    fi
+    # 
 
-    if [ "$component" = "upgrade-eks-nodepools" ]; then
-        handle_terraform_nodepools
-    fi
-    
-    if [ "$component" = "upgrade-kubernetes" ]; then
-        handle_kubernetes_version
+    if [ "$component" = "aws" ]; then
+        handle_aws_options
     fi
 
     if [ "$component" = "glueops-platform" ]; then
