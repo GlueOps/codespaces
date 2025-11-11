de7dd61251516ff292a716795d84665576cd6aec
Venkat <venkata@venkatamutyala.com>
2025-11-11 00:22:54 -0800

    refactor: fixes an issue where if gh auth login is run manually the git email, git name were not being configured correctly (#403)

--- .devcontainer/tools/yolo.sh
+++ .devcontainer/tools/yolo.sh
@@ -10,16 +10,18 @@ if [[ ! -f /home/vscode/.config/gh/hosts.yml ]] || ! gh auth status &>/dev/null;
     # Run the gh auth login command
     yes Y | gh auth login -h github.com -p https -w -s repo,workflow,admin:org,write:packages,user,gist,notifications,admin:repo_hook,admin:public_key,admin:enterprise,audit_log,codespace,project,admin:gpg_key,admin:ssh_signing_key
 
-    echo "Set up git with gh auth"
-    gh auth setup-git
-
-    # Configure git config
-    git_email=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user/emails | jq '.[] | select(.primary == true) | .email' -r)
-    git_name=$(gh api user | jq '.name' -r)
-    git config --global user.email "${git_email}"
-    git config --global user.name "${git_name}"
-    git config --global core.autocrlf input
 else
     echo "GitHub authentication is already set up."
     gh auth status
 fi
+
+
+# Set up git with gh auth
+gh auth setup-git
+
+# Configure git config
+git_email=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /user/emails | jq '.[] | select(.primary == true) | .email' -r)
+git_name=$(gh api user | jq '.name' -r)
+git config --global user.email "${git_email}"
+git config --global user.name "${git_name}"
+git config --global core.autocrlf input
